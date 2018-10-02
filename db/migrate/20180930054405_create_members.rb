736425779e07bed75008c50b7c2c4376ab802b37
yamazaki27 <mr.benature.children.y.y@gmail.com>
2018-10-02 15:21:25 +0900

    Edit message-system

--- db/migrate/20180930054405_create_members.rb
+++ db/migrate/20180930054405_create_members.rb
@@ -1,4 +1,5 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
+
   def change
     create_table :members do |t|
       t.references :user, foreign_key: true
@@ -6,4 +7,5 @@ class CreateMembers < ActiveRecord::Migration[5.0]
       t.timestamps
     end
   end
+
 end
