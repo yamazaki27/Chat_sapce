f2802baa020c1ea2015751ca5dd8255fbcef2794
yamazaki27 <mr.benature.children.y.y@gmail.com>
2018-10-09 19:56:50 +0900

    Edit search system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?) and name != ?', "%#{params[:keyword]}%", current_user.name)
     respond_to do |format|
       format.json
     end
