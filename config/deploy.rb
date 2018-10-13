0640f4ea8805852e22efe1be5aa6b4f02072d178
yamazaki27 <mr.benature.children.y.y@gmail.com>
2018-10-13 17:11:14 +0900

    Edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.11.0'
 
 set :application, 'chat_space'
 set :repo_url,  'git@github.com:yamazaki27/Chat_space.git'
