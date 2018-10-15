cc25ec759548b95b5563c491215a6879ae1f2a97
yamazaki27 <mr.benature.children.y.y@gmail.com>
2018-10-15 12:41:10 +0900

    edit set application-name2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock '3.11.0'
 
-set :application, 'chat_space'
+set :application, 'Chat_sapce'
 set :repo_url,  'git@github.com:yamazaki27/Chat_sapce.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
